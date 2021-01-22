5fa16354d9a332fe3b8ae00bbba30a606bd36d8c
Mohammad Raza <qarar@Razas-MacBook-Air.local>
2021-01-22 17:20:05 +0530

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,41 @@
+Last login: Fri Jan 22 15:46:56 on console
+qarar@Razas-MacBook-Air ~ % git version
+git version 2.30.0
+qarar@Razas-MacBook-Air ~ % cd Desktop
+qarar@Razas-MacBook-Air Desktop % cd LearngitRepo/
+qarar@Razas-MacBook-Air LearngitRepo % ls
+qarar@Razas-MacBook-Air LearngitRepo % git init
+hint: Using 'master' as the name for the initial branch. This default branch name
+hint: is subject to change. To configure the initial branch name to use in all
+hint: of your new repositories, which will suppress this warning, call:
+hint: 
+hint: 	git config --global init.defaultBranch <name>
+hint: 
+hint: Names commonly chosen instead of 'master' are 'main', 'trunk' and
+hint: 'development'. The just-created branch can be renamed via this command:
+hint: 
+hint: 	git branch -m <name>
+Initialized empty Git repository in /Users/qarar/Desktop/LearnGitRepo/.git/
+qarar@Razas-MacBook-Air LearngitRepo % git status
+On branch master
+
+No commits yet
+
+nothing to commit (create/copy files and use "git add" to track)
+qarar@Razas-MacBook-Air LearngitRepo % 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
